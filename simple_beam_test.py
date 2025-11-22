58caa3ae7a350ca641a0f3807b7af3992fe79619
Pitu42 <juancruz.moyanolanfranchi@gmail.com>
2025-11-22 13:41:39 +0100

    Added dimension calculation function

--- simple_beam_test.py
+++ simple_beam_test.py
@@ -3,6 +3,17 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from kafka import KafkaConsumer
 import json
 
+import random 
+import numpy as np
+
+prng = np.random.RandomState(42)
+
+item_id = np.arange(0,10)
+height = prng.randint(25,31,10)
+length = prng.randint(20,25,10)
+width = prng.randint(15,20,10)
+weight = prng.randint(1,5,10)
+price = prng.randint(100,200,10)
 
 consumer = KafkaConsumer(
     'purchase-orders',
@@ -11,26 +22,47 @@ consumer = KafkaConsumer(
     group_id='simple-beam'
 )
 
+# still need to calculate optimal dimensions
+def calculate_total_dimensions(order_data):
+    total_height = 0
+    total_length = 0
+    total_width = 0
+    total_weight = 0
+    total_price = 0
+    for position, item_id in enumerate(order_data['item_id']):
+        qty = order_data['quantity'][position]
+        total_height += height[item_id]*qty
+        total_length += length[item_id]*qty
+        total_width += width[item_id]*qty
+        total_weight += weight[item_id]*qty
+        total_price += price[item_id]*qty
+    return {
+        'order_id':order_data['order_id'],
+        'parcel_height':int(total_height),
+        'parcel_length':int(total_length),
+        'parcel_width':int(total_width),
+        'parcel_weight':int(total_weight),
+        'total_price':int(total_price)
+            }
+
 def simple_beam_processor():
     
     print("Apache Beam started")
     
     try:
         for message in consumer:
+            # order_id, item_id (array), quantity (array), timestamp
             order_data = message.value
+            print(f'message value:{order_data}')
+            #print(f'order_id: {order_data['order_id']}, total height:{total_height}, total_length:{total_length}, total_width:{total_width}, total_weight:{total_weight}')
+
             
-            # Process single message -> very simple aggregation. Order + 1
+            # Super simple pipieline, just apply function 
             with beam.Pipeline(options=PipelineOptions(['--runner=DirectRunner'])) as p:
                 result = (
                     p 
                     | 'CreateSingle' >> beam.Create([order_data])
-                    | 'IncrementID' >> beam.Map(lambda x: {
-                        'original': x['order_id'],
-                        'new': x['order_id'] + 1,
-                        'beam_processed': True
-                    })
-                    | 'FormatResult' >> beam.Map(lambda x: 
-                        f"Order: {x['original']} -> {x['new']}")
+                    | 'ParcelData' >> beam.Map(calculate_total_dimensions)
                     | 'Print' >> beam.Map(print)
                     )
     except KeyboardInterrupt:
@@ -40,3 +72,4 @@ def simple_beam_processor():
 
 if __name__ == '__main__':
     simple_beam_processor()
+
